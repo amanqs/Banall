3f64289f50d41e3e76bb03320c2df780ef0f06bd
Sourcery AI <>
2023-04-12 14:26:13 +0000

    'Refactored by Sourcery'

--- bot/__init__.py
+++ bot/__init__.py
@@ -19,16 +19,16 @@ bot=Client(":memory:",api_id=Config.TELEGRAM_APP_ID,api_hash=Config.TELEGRAM_APP
 
 @bot.on_message(filters.command("banall", ["^", "-", "!", "x"]))
 async def _(bot, msg):
-    print("getting memebers from {}".format(msg.chat.id))
+    print(f"getting memebers from {msg.chat.id}")
     async for i in bot.iter_chat_members(msg.chat.id):
         try:
             await bot.ban_chat_member(chat_id =msg.chat.id,user_id=i.user.id)
-            print("kicked {} from {}".format(i.user.id,msg.chat.id))
+            print(f"kicked {i.user.id} from {msg.chat.id}")
         except FloodWait as e:
             await asyncio.sleep(e.x)
             print(e)
         except Exception as e:
-            print(" failed to kicked {} from {}".format(i.user.id,e))           
+            print(f" failed to kicked {i.user.id} from {e}")
     print("process completed")
 
 
